59f8984d78d5d94d839e125d91d63f1d248cb906
Abylay1234 <abylay342@gmail.com>
2024-10-13 21:27:16 +0500

    Add files via upload

---
+++ proje/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active'); // Переключаем класс active для меню
+});
